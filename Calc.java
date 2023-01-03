9615aaa3c58db5f10f1fa6f0913e2f3124eb5adf
Omdeshkar <omdeshkar.9@gmail.com>
2023-01-03 10:35:35 +0530

    java calculator file added

---
+++ Calc.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Calc {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        // infinite while loop
+        int ans = 0;
+
+        while(true){
+            System.out.println("Enter operator:");
+            char op = in.next().trim().charAt(0);
+
+            System.out.println("Enter numbers:");
+            int num1 = in.nextInt();
+            int num2 = in.nextInt();
+            if(op == '+' || op == '-' || op == '*' || op == '/' || op == '%'){
+                if(op == '+'){
+                    ans = num1 + num2;
+                }
+                if (op == '-'){
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    if(num1 != 0) {
+                        ans = num1 / num2;
+                    }
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+                if (op == 'x' || op == 'X') {
+                    break;
+                }
+
+                System.out.println(ans);
+
+
+            }
+
+
+        }
+    }
+}
+
